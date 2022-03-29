d9e851ad4e58742e1afe8cfc80fe73204bb39303
JinzhaoChen <j.chen88@newcastle.ac.uk>
2022-03-29 22:44:20 +0100

    first commit

---
+++ src/a_基础操作/HelloWorld.scala
@@ -0,0 +1,13 @@
+package a_基础操作
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+
+//    val sparkConfig = new SparkConfig().setMaster("local").setAppName("Worldcount");
+//    val sc = new SparkContext();
+//
+//
+//    sc.stop();
+  }
+}
